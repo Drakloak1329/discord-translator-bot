b99a004318b5413d32ac56212206fa13d42c1a82
SuperUser <68007068+secretgithubuser@users.noreply.github.com>
2020-08-25 08:54:28 +0000

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,3 +1,15 @@
 #!/usr/bin/python3
 import discord as d
+from discord.ext import commands as com
 import googletrans as g
+
+bot= com.Bot(prefix="translate",description="翻訳者ボットは素晴らしいですね。, Use 'translator text <text>' to translate.")
+
+
+@bot.event
+async def on_ready():
+  print(">>> Logged in as "+bot.user.name)
+
+@bot.command()
+aysnc def no_command(ctx):
+  
