f3d1e72d365fc59ae0643aeb2bc16002a4f27677
The Scribe <68007068+lazylavenders@users.noreply.github.com>
2020-12-20 08:57:52 +0000

    Update Translator.py

--- Translator.py
+++ Translator.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 import discord as d
 from discord.ext import commands as com
-import googletrans as g
+import googletrans as gTrans
 
 bot= com.Bot(command_prefix="translate",description="翻訳者ボットは素晴らしいですね。, Use 'translator text <text>' to translate.")
-trn=g.Translator()
-lng=g.LANGUAGES
-tok="NzM2NjA2NDkxMzU1NjQzOTU0.XxxQQQ.JyIDQ3O8uXPkMqiE01GVYN2Cq34"
+translator=gTrans.Translator()
+langs=gTrans.LANGUAGES
+token="NzM2NjA2NDkxMzU1NjQzOTU0.XxxQQQ.JyIDQ3O8uXPkMqiE01GVYN2Cq34"
 
 @bot.event
 async def on_ready():
@@ -18,7 +18,7 @@ async def no_command(ctx):
 
 @bot.command(" text")
 async def on_text(ctx, args):
-  tran=trn.translate(text)
-  await bot.say(f"From : {lang[tran.src]}\nText : `{tran.text.escape()}`")
+  tran=trans.translate(text)
+  await bot.say(f"From : {langs[tran.src]}\nText : `{tran.text.escape()}`")
 
-bot.run(tok)
+bot.run(token)
